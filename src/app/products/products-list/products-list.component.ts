c2f78a28ce8b4e3912bd0baffdc247225c62beec
MorenoLucas <sl.lucasmoreno@gmail.com>
2021-03-29 18:03:36 +0200

    recuperamos la lista de productos y la enlazamos y mostramos

--- src/app/products/products-list/products-list.component.ts
+++ src/app/products/products-list/products-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../shared/services/products.service';
+import { Product } from '../shared/models/product';
 
 @Component({
   selector: 'ed-products-list',
   templateUrl: './products-list.component.html',
-  styleUrls: ['./products-list.component.css']
+  styleUrls: ['./products-list.component.css'],
 })
 export class ProductsListComponent implements OnInit {
+  products: Product[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private service: ProductsService) {}
+  // AQUI LOS AGARRAMOS DEL SERVICIO
+  ngOnInit() {
+    this.service.getAll().subscribe((data) => {
+      console.log('data', data);
+      this.products = data;
+    });
   }
-
 }
