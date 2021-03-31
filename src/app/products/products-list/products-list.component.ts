e9998696caa4c5764d2b858786a000d42962b870
MorenoLucas <sl.lucasmoreno@gmail.com>
2021-03-31 17:00:32 +0200

    creamos dialogo con mensaje

--- src/app/products/products-list/products-list.component.ts
+++ src/app/products/products-list/products-list.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../shared/services/products.service';
 import { Product } from '../shared/models/product';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../../shared/components/confirm-dialog/confirm-dialog.component';
+import { ConfirmDialogModel } from 'src/app/shared/models/confirm-dialog-model';
 
 @Component({
   selector: 'ed-products-list',
@@ -13,25 +16,41 @@ export class ProductsListComponent implements OnInit {
 
   constructor(
     private service: ProductsService,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private dialog: MatDialog
   ) {}
   // AQUI LOS AGARRAMOS DEL SERVICIO
   ngOnInit() {
     this.loadProducts();
   }
   deleteProduct(product: Product) {
-    this.service.delete(product.id).subscribe((response) => {
-      console.log('producto borrado', response);
-      this.loadProducts();
-      this.snackBar.open('Producto Borrado', 'Close', {
-        duration: 3000,
-      });
+    // recibe el dialogo el componente dialogo y le especificamos titulo y mensaje
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      maxWidth: '600px',
+      data: <ConfirmDialogModel>{
+        title: 'Borrar Producto',
+        message: 'Estas seguro de querer borrar el producto?',
+      },
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.sentDeleterequest(product);
+      }
     });
   }
   private loadProducts() {
     this.service.getAll().subscribe((data) => {
       console.log('data', data);
       this.products = data;
+    });
+  }
+  private sentDeleterequest(product: Product) {
+    this.service.delete(product.id).subscribe((response) => {
+      console.log('producto borrado', response);
+      this.loadProducts();
+      this.snackBar.open('Producto Borrado', 'Close', {
+        duration: 3000,
+      });
     });
   }
 }
--- src/app/shared/models/confirm-dialog-model.ts
+++ src/app/shared/models/confirm-dialog-model.ts
@@ -1,4 +1,4 @@
 export interface ConfirmDialogModel {
   title: string;
-  massage: string;
+  message: string;
 }
