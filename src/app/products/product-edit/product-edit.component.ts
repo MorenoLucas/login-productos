9641196fd466057b4d61cd85d0bd3aa9b6b72c75
MorenoLucas <sl.lucasmoreno@gmail.com>
2021-04-02 12:50:59 +0200

    mensaje de error

--- src/app/products/product-edit/product-edit.component.ts
+++ src/app/products/product-edit/product-edit.component.ts
@@ -4,6 +4,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ProductsService } from '../shared/services/products.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Product } from '../shared/models/product';
+import { catchError } from 'rxjs/operators';
+import { EMPTY } from 'rxjs';
 
 @Component({
   selector: 'ed-product-edit',
@@ -32,13 +34,27 @@ export class ProductEditComponent implements OnInit {
     const product = this.product;
     product.id = this.id;
     console.log('actualizamos', product);
-    this.service.update(product).subscribe((result) => {
-      console.log('actualizamos', result);
-      this.router.navigate(['/product']);
-      this.snackBar.open('Producto Actualizado', 'Cerrar', {
-        duration: 3000,
+    this.service
+      .update(product)
+      .pipe(
+        catchError((error) => {
+          this.snackBar.open(
+            'No podemos obtener el detalle del produto',
+            null,
+            {
+              duration: 3000,
+            }
+          );
+          return EMPTY;
+        })
+      )
+      .subscribe((result) => {
+        console.log('actualizamos', result);
+        this.router.navigate(['/product']);
+        this.snackBar.open('Producto Actualizado', 'Cerrar', {
+          duration: 3000,
+        });
       });
-    });
   }
   cancel() {
     this.router.navigate(['/product']);
