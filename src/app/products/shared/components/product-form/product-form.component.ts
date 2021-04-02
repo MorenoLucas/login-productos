86e2d4724f5972336488a3044d93451012343fc2
MorenoLucas <sl.lucasmoreno@gmail.com>
2021-04-02 12:06:16 +0200

    agregamos mat-error para el html

--- src/app/products/shared/components/product-form/product-form.component.ts
+++ src/app/products/shared/components/product-form/product-form.component.ts
@@ -1,5 +1,11 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { Product } from '../../models/product';
 
 @Component({
@@ -8,12 +14,18 @@ import { Product } from '../../models/product';
   styleUrls: ['./product-form.component.css'],
 })
 export class ProductFormComponent implements OnInit {
+  // validador de URL
+  urlPattern = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/;
+
   form: FormGroup = new FormGroup({
-    title: new FormControl(''),
-    brand: new FormControl(''),
-    price: new FormControl(''),
-    salePrice: new FormControl(''),
-    thumbImage: new FormControl(''),
+    title: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    brand: new FormControl('', Validators.required),
+    price: new FormControl('', [Validators.required, this.minPrice(10)]),
+    salePrice: new FormControl('', [Validators.required, this.minPrice(10)]),
+    thumbImage: new FormControl('', [
+      Validators.required,
+      Validators.pattern(this.urlPattern),
+    ]),
   });
   @Input() title: string;
   @Input() labelSubmit: string;
@@ -44,5 +56,16 @@ export class ProductFormComponent implements OnInit {
   }
   onCancel() {
     this.cancel.emit();
+  }
+  private minPrice(minPrice: number): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      if (control.value !== undefined && control.value <= minPrice) {
+        return {
+          minprice: true,
+        };
+      } else {
+        return null;
+      }
+    };
   }
 }
